5e6d9c035de940b40cec98baaab8c74afa3f1984
vincent-tr <vincent.tr@gmail.com>
2019-05-28 15:04:14 +0200

    cleanup

--- src/components/application/index.js
+++ src/components/application/index.js
@@ -1,5 +1,3 @@
 'use strict';
 
-import './main.scss';
-
 export Application from './application';
--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,6 @@
 import 'normalize.css';
 import 'ubuntu-fontface/_ubuntu-base.scss';
 
-export * from './icon';
-export * from './button';
 export * from './editors';
 export * from './containers';
 export * from './layout';
