f56c597be95755cd588fbcffd14a85ea06a229e8
vincent-tr <vincent@stoic.com>
2019-05-30 17:31:13 +0200

    update bootstrap

--- src/components/application/application.js
+++ src/components/application/application.js
@@ -1,23 +1,48 @@
 'use strict';
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+import { colors, createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
 import StoreProvider from './store-provider';
 import Dialogs from '../../modules/dialogs/components/dialogs';
 //import { LayoutRouter } from '../../modules/routing/components';
 import { Layout } from '../layout';
 
-const Application = ({ routes, ...props }) => (
-  <StoreProvider>
-    <React.Fragment>
-      <Dialogs />
-      <Layout />
-      {/*
-      <LayoutRouter {...props}>
-        {routes}
-      </LayoutRouter>
-      */}
-    </React.Fragment>
-  </StoreProvider>
+const defaultThemeConfig = {
+  palette: {
+    primary: colors.blue,
+    secondary: colors.pink,
+  }
+};
+
+const Application = ({
+  routes,
+  theme = createMuiTheme(defaultThemeConfig),
+  ...props
+} = {}) => (
+  <MuiPickersUtilsProvider utils={MomentUtils}>
+    <MuiThemeProvider theme={theme}>
+      <StoreProvider>
+        <React.Fragment>
+          <Dialogs />
+          <Layout />
+          {/*
+          <LayoutRouter {...props}>
+            {routes}
+          </LayoutRouter>
+          */}
+        </React.Fragment>
+      </StoreProvider>
+    </MuiThemeProvider>
+  </MuiPickersUtilsProvider>
 );
+
+Application.propTypes = {
+  routes: PropTypes.oneOfType([ PropTypes.arrayOf(PropTypes.node), PropTypes.node ]),
+  theme: PropTypes.object
+};
 
 export default Application;
