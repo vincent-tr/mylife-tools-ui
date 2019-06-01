123ad130496e8ee4eb724323015379c13eed9a7b
vincent-tr <vincent@stoic.com>
2019-06-01 14:54:12 +0200

    export stuff directly

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-export { default as React } from 'react';
-export * from 'react';
-export { default as PropTypes } from 'prop-types';
-export { default as clsx } from 'clsx';
-
-export * from '@material-ui/core';
-export * from '@material-ui/pickers';
-export * as icons from '@material-ui/icons';
 
 export * from './layout';
 export * from './editors';
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@
 import 'typeface-roboto';
 import 'material-icons/iconfont/material-icons.css';
 
-export * as components from './components';
+export { default as React } from 'react';
+export * from 'react';
+export { default as PropTypes } from 'prop-types';
+export { default as clsx } from 'clsx';
+
+export * from '@material-ui/core';
+export * from '@material-ui/pickers';
+export * as icons from '@material-ui/icons';
+
+export * from 'redux-actions';
+
+export * from './components';
 export * as constants from './constants';
 export * as services from './services';
 export * from './modules';
