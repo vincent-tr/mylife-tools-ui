841c3459404ea12e101d8a3845d4ad79d16bad4d
vincent-tr <vincent@stoic.com>
2019-06-06 13:35:00 +0200

    oops

--- src/modules/dialogs/components/confirm.js
+++ src/modules/dialogs/components/confirm.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from '@material-ui/core';
 
 const Confirm = ({ show, proceed, options }) => (
-  <Dialog aria-labelledby='simple-dialog-title' open={show} scroll='paper'>
+  <Dialog aria-labelledby='dialog-title' open={show} scroll='paper'>
     {options.title && (
       <DialogTitle id='dialog-title'>
         {options.title}
