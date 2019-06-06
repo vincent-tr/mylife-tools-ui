a58042b34f475066a1366a7671f870a7d6b79627
vincent-tr <vincent@stoic.com>
2019-06-06 17:37:42 +0200

    input dialog

---
+++ src/modules/dialogs/components/input.js
@@ -0,0 +1,40 @@
+'use strict';
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, TextField } from '@material-ui/core';
+
+const Confirm = ({ show, proceed, options }) => {
+  const [text, setText] = useState(options.text);
+  return (
+    <Dialog aria-labelledby='dialog-title' open={show} scroll='paper' maxWidth='sm' fullWidth>
+      {options.title && (
+        <DialogTitle id='dialog-title'>
+          {options.title}
+        </DialogTitle>
+      )}
+
+      <DialogContent dividers>
+        {options.message && (
+          <DialogContentText>
+            {options.message}
+          </DialogContentText>
+        )}
+        <TextField autoFocus fullWidth label={options.label} id='text' value={text || ''} onChange={e => setText(e.target.value)} />
+      </DialogContent>
+
+      <DialogActions>
+        <Button color='primary' onClick={() => proceed(text)}>OK</Button>
+        <Button onClick={() => proceed()}>Annuler</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+Confirm.propTypes = {
+  show: PropTypes.bool,
+  proceed: PropTypes.func,
+  options: PropTypes.object
+};
+
+export default Confirm;
--- src/modules/dialogs/helpers.js
+++ src/modules/dialogs/helpers.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { confirmable, createConfirmation } from 'react-confirm';
 import Confirm from './components/confirm';
+import Input from './components/input';
 import { ToolsProvider } from '../../components/application';
 
 export function create(Dialog) {
@@ -18,12 +19,18 @@ export function create(Dialog) {
 
 const confirmDialog = create(Confirm);
 
-const defaultActions = [
+const defaultConfirmActions = [
   { text: 'Oui', value: true },
   { text: 'Non', value: false }
 ];
 
 export async function confirm(options) {
-  options.actions = options.actions || defaultActions;
+  options.actions = options.actions || defaultConfirmActions;
   return confirmDialog({ options });
+}
+
+const inputDialog = create(Input);
+
+export async function input(options) {
+  return inputDialog({ options });
 }
