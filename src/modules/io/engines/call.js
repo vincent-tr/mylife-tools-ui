7c058fb274fab22a00516efe80a0f663b1a5f9ba
vincent-tr <vincent@stoic.com>
2019-06-19 16:22:23 +0200

    wip socket engines

---
+++ src/modules/io/engines/call.js
@@ -0,0 +1,79 @@
+'use strict';
+
+import { utils } from 'mylife-tools-common';
+
+const CALL_TIMEOUT = 5000;
+
+class Pending {
+  constructor(engine, transaction, deferred) {
+    this.engine = engine;
+    this.transaction = transaction;
+    this.deferred = deferred;
+    this.timeout = setTimeout(() => this.onTimeout(), CALL_TIMEOUT);
+  }
+
+  finish(error, result) {
+    clearTimeout(this.timeout);
+    this.engine.pendings.delete(this.transaction);
+    if(error) {
+      this.deferred.reject(error);
+    } else {
+      this.deferred.resolve(result);
+    }
+  }
+
+  reply(message) {
+    const { error, result } = message;
+    this.finish(error, result);
+  }
+
+  onDisconnect() {
+    this.finish(new Error('Disconnected while running query'));
+  }
+
+  onTimeout() {
+    this.finish(new Error('Timeout while running query'));
+  }
+}
+
+class CallEngine {
+  constructor(emitter, dispatch) {
+    this.emitter = emitter;
+    this.dispatch = dispatch;
+    this.pendings = new Map();
+    this.transactionCounter = 0;
+  }
+
+  onDisconnect() {
+    const pendings = Array.from(this.pendings.values());
+    for(const pending of pendings) {
+      pending.onDisconnect();
+    }
+  }
+
+  onConnect() {
+  }
+
+  onMessage(message) {
+    const { transaction } = message;
+    const pending = this.pendings.get(transaction);
+    if(!pending) {
+      console.log('Got response for unknown transaction, ignored');
+      return;
+    }
+
+    pending.reply(message);
+  }
+
+  async call(message) {
+    const transaction = ++this.transactionCounter;
+    this.emitter({ ...message, transaction });
+
+    const deferred = utils.defer();
+    this.pendings.set(transaction, new Pending(this, transaction, deferred));
+
+    return await deferred.promise;
+  }
+}
+
+export default CallEngine;
--- src/modules/io/middleware.js
+++ src/modules/io/middleware.js
@@ -3,17 +3,32 @@
 import io from 'socket.io-client';
 import actionTypes from './action-types';
 import { setOnline } from './actions';
+import { serializer } from 'mylife-tools-common';
+import CallEngine from './engines/call';
 
 export default (/*store*/) => next => {
 
   const socket = io();
+  const emitter = (message) => socket.emit('message', serializer.serializer(message));
+
+  const engines = {
+    call: new CallEngine(emitter)
+  };
 
   socket.on('connect', () => {
+    for(const engine of Object.values(engines)) {
+      engine.onConnect();
+    }
+
     next(setOnline(true));
   });
 
   socket.on('disconnect', (reason) => {
     next(setOnline(false));
+
+    for(const engine of Object.values(engines)) {
+      engine.onDisconnect();
+    }
 
     // failure on network === 'transport closed'
     if(reason === 'io server disconnect') {
@@ -22,13 +37,20 @@ export default (/*store*/) => next => {
     }
   });
 
-  socket.on('message', payload => console.log(deserialize(payload)));
+  socket.on('message', payload => {
+    const message = serializer.deserialize(payload);
+    const engine = engines[message.engine];
+    if(!engine) {
+      console.log(`Message with unknown engine '${message.engine}', ignored`);
+      return;
+    }
+
+    engine.onMessage(message);
+  });
 
   return action => {
+    // TODO: handle calls
+    
     return next(action);
   };
 };
-
-function deserialize(payload) {
-  return payload;
-}
