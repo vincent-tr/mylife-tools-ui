9ee7bb307d4b8da8f6c76cfc5baf26883cc36f00
vincent-tr <vincent@stoic.com>
2019-07-31 15:12:24 +0200

    viewSelectors

--- src/modules/io/selectors.js
+++ src/modules/io/selectors.js
@@ -6,3 +6,5 @@ const getIo = state => getInternalState(state).io;
 
 export const getOnline = state => getIo(state).online;
 export const getView = (state, viewId) => getIo(state).views.get(viewId);
+export const getViewItem = (state, viewId, itemId) => getIo(state).views.get(viewId).get(itemId);
+export const getViewList = (state, viewId) => getIo(state).views.get(viewId).valueSeq().toArray();
