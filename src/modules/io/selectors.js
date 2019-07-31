5e3f0c43c7bc29a7e38db74cec5614695b125605
vincent-tr <vincent@stoic.com>
2019-07-31 15:26:23 +0200

    add view selector

--- src/modules/io/selectors.js
+++ src/modules/io/selectors.js
@@ -1,10 +1,13 @@
 'use strict';
 
+import Immutable from 'immutable';
 import { getInternalState } from '../../selectors/base';
 
 const getIo = state => getInternalState(state).io;
 
+const emptyView = new Immutable.Map();
+
 export const getOnline = state => getIo(state).online;
-export const getView = (state, viewId) => getIo(state).views.get(viewId);
-export const getViewItem = (state, viewId, itemId) => getIo(state).views.get(viewId).get(itemId);
-export const getViewList = (state, viewId) => getIo(state).views.get(viewId).valueSeq().toArray();
+export const getView = (state, viewId) => getIo(state).views.get(viewId) || emptyView;
+export const getViewItem = (state, viewId, itemId) => getView(state, viewId).get(itemId);
+export const getViewList = (state, viewId) => getView(state, viewId).valueSeq().toArray();
